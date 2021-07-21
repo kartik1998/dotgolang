b12f604cdc978c38fb367c73c3e709071e781e97
Kartik <kartik@hyperverge.co>
2021-07-22 01:53:45 +0530

    Upate structs

--- structs/main.go
+++ structs/main.go
@@ -2,17 +2,25 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
 type person struct {
 	firstname string
 	lastname  string
+	contact   contactInfo
 }
 
 func main() {
 	// alex := person{firstname: "Alex", lastname: "Anderson"}
 	// fmt.Println(alex)
-	var alex person
-	alex.firstname = "Alex"
-	alex.lastname = "Anderson"
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	// var alex person
+	// alex.firstname = "Alex"
+	// alex.lastname = "Anderson"
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+
+	jim := person{firstname: "Jim", lastname: "Halls", contact: contactInfo{email: "jim@gmail.com", zipCode: 95347}}
+	fmt.Printf("%+v", jim)
 }
