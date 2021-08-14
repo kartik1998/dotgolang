a8d0a134e03813541d4261fbaa77fdec37be393a
Kartik <kartik@hyperverge.co>
2021-08-14 22:54:55 +0530

    Update: serving html pages folder structure

---
+++ udemy/serving_html_templates/handlers.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+// Home is the handler for the home page
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.tmpl")
+}
+
+// About is the handler for the about page
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.tmpl")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template:", err)
+	}
+}
